1c594b2c155e40d2ff690d4cb92af00778dfd373
J Martin <info@janetmartin.ca>
2022-07-12 23:03:49 -0700

    Remove idea

--- main.py
+++ main.py
@@ -4,17 +4,6 @@
 # data from http://waqi.info/
 # description: a python script to check the local air quality index (AQI) and set the colour of a string of WS2812B LEDs
 
-# Helpful resources:
-# https://www.w3schools.com/python/python_json.asp
-# https://www.w3schools.com/python/module_requests.asp
-# https://www.w3schools.com/python/python_dictionaries.asp
-# https://www.programiz.com/python-programming/nested-dictionary
-# https://www.askpython.com/python/examples/pull-data-from-an-api
-# https://oxylabs.io/blog/python-parse-json
-# https://aqicn.org/json-api/doc/#api-Geolocalized_Feed-GetHereFeed
-# https://restfulapi.net/
-
-
 # import modules
 import requests
 import json
@@ -22,7 +11,7 @@ import json
 # import API url and key
 import constants
 
-# Get Data
+# GET DATA
 response = requests.get(constants.API_url)  # connect to api url and get response object
 resp_json = json.loads(response.text)  # loads json response object as a Python dictionary
 # print(type(resp_json)) #uncomment to check type
@@ -51,5 +40,6 @@ elif aqi in range(200, 301):
     print('The air quality is very unhealthy.')
 else:
     print('The air quality is hazardous.')
+
 # Next: code to light up WS2812b string running of pizero data pin
 # Reminder re: setup https://core-electronics.com.au/guides/ws2812-addressable-leds-raspberry-pi-quickstart-guide/
