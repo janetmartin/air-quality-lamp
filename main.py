ccfdf2febf7ccd79498583369dd9e50d73257178
J Martin <info@janetmartin.ca>
2022-08-05 22:24:47 -0700

    Switched to purple air API.

--- main.py
+++ main.py
@@ -1,34 +1,102 @@
+from encodings import undefined
 import requests
+import json
+import math
 
 # import api key from client-side file
 from constants import my_api_read_key
-# To use your API key in this file, comment out the line above, and uncomment the line below.
-# my_api_read_key = 'YOU API KEY HERE'
+
+# my_api_read_key = 'YOUR API KEY HERE'
 
 # STEP 1: function to send get request to PurpleAir
-def getSensorData(sensor_index):
-    # my_url is assigned the URL we are going to send our request to.
+# Note: the PurpleAir Map uses the 10minute PM2.5 average to calculate AQI, not the point value.
+
+def get_sensor_data(sensor_index):
+
+    # send data request
     my_url = ('https://api.purpleair.com/v1/sensors/' + str(sensor_index))
+    my_headers = {'X-API-Key': my_api_read_key}  # PurpleAir prefers the headers method
+    response = requests.get(my_url, headers=my_headers)  # response assigned to variable
 
-    # my_headers is assigned the context of our request we want to make. In this case
-    # we will pass through our API read key using the variable created above.
-    my_headers = {'X-API-Key': my_api_read_key}
-
-    # This line creates and sends the request and then assigns its response to the
-    # variable, r.
-    response = requests.get(my_url, headers=my_headers)
-
-    # We then return the response we received.
+    # check status codes.
     if response.status_code == 200:
         print('Success!')
     elif response.status_code == 404:
         print('Not Found.')
-    print(response.text)
-    return response
 
-# STEP 2: function to calculate AQI
-# STEP 3: function to control leds
+    # load response as Python object
+    pm = json.loads(response.text)['sensor']['stats']['pm2.5_10minute']
 
-# EXAMPLE:See map for locations https://map.purpleair.com/ and retrieve station # from URLS
-getSensorData(129299) #this is the number of the station in George Wainborn Park, Vancouver, BC
+    #check if data is valid valueS
 
+    return pm
+
+# check that pm is a valid reading
+def validate_pm():
+    if math.isnan(pm):
+        return "-"
+    elif pm == 'undefined':
+        return "-"
+    elif pm < 0:
+        return pm
+    elif (pm > 1000):
+        return "-"
+
+# USA EPA Formula for AQI
+def calc_aqi(pm, Ih, Il, BPh, BPl):
+    a = Ih - Il
+    b = BPh - BPl
+    c = pm - BPl
+    return round((a / b) * c + Il)
+
+# Actual API Calcuation using the USA EPA breakpoint values
+def aqi(pm):
+    if pm > 350.5:
+        return calc_aqi(pm, 500, 401, 500.4, 350.5)  # Hazardous
+    elif pm > 250.5:
+        return calc_aqi(pm, 400, 301, 350.4, 250.5)  # Hazardous
+    elif pm > 150.5:
+        return calc_aqi(pm, 300, 201, 250.4, 150.5)  # Very Unhealthy
+    elif pm > 55.5:
+        return calc_aqi(pm, 200, 151, 150.4, 55.5)  # Unhealthy
+    elif pm > 35.5:
+        return calc_aqi(pm, 150, 101, 55.4, 35.5)  # Unhealthy for Sensitive Groups
+    elif pm > 12.1:
+        return calc_aqi(pm, 100, 51, 35.4, 12.1)  # Moderate
+    elif pm >= 0:
+        return calc_aqi(pm, 50, 0, 12, 0)  # Good
+    else:
+        return undefined
+
+
+# SCALE: World Air Quality Index Rating Scale
+# air quality scale from WAQI
+# 0 - 50 Good (green)
+# 51 - 100 Moderate (yellow)
+# 101 - 150 Unhealthy for Sensitive Groups (orange)
+# 151 - 200 Unhealthy (red)
+# 201 - 300 Very Unhealthy (purple)
+# 300+ Hazardous (burgundy)
+
+# logic based on value of aqi
+def aqi_ranking(aqi):
+    if aqi in range(0, 51):
+        print('The air quality is good.')
+    elif aqi in range(51, 101):
+        print('The air quality is moderate.')
+    elif aqi in range(101, 150):
+        print('The air quality is unhealthy for sensitive groups.')
+    elif aqi in range(151, 200):
+        print('The air quality is unhealthy.')
+    elif aqi in range(200, 301):
+        print('The air quality is very unhealthy.')
+    else:
+        print('The air quality is hazardous.')
+
+# VALUE FOR SELECTED SENSOR STATION
+# Enter the desired station number from map.purpleair.com
+pm = get_sensor_data(12999)
+aqi= aqi(pm)
+print('The 10-minute average particulate value in George Waiborn Park is: ' + str(pm))
+print('The current air quality index is: ' + str(aqi))
+report= aqi_ranking(aqi)
